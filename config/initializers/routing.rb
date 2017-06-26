5d668c6140056e426bb5dc6f984604967c7cc347
Yaroslav Konoplov <eahome00@gmail.com>
2017-06-26 16:39:00 +0300

    initializers/routing.rb: configure all rails URL options in one place, AIO. Fixing many misunderstandings I think :)

---
+++ config/initializers/routing.rb
@@ -0,0 +1,11 @@
+Rails.application.configure do
+  { host:           SC.url.host,
+    port:           SC.url.port,
+    protocol:       SC.url.protocol,
+    trailing_slash: true
+  }.tap do |url_options|
+    config.action_controller.default_url_options = url_options
+    config.action_mailer.default_url_options     = url_options
+    routes.default_url_options                   = url_options
+  end
+end
