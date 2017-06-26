ea4bdf004e4e73c8cf7428cac4c5d1a5d9f3327f
Yaroslav Konoplov <eahome00@gmail.com>
2017-06-26 16:27:42 +0300

    1. Disable sprockets precompiled assets checks. 2. Force asset host.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,11 +1,11 @@
-# Be sure to restart your server when you modify this file.
+Rails.application.configure do
+  # Version of your assets, change this if you want to expire all your assets.
+  config.assets.version                 = '1.0'
+  config.assets.check_precompiled_asset = false
 
-# Version of your assets, change this if you want to expire all your assets.
-Rails.application.config.assets.version = '1.0'
+  # Precompile additional assets.
+  config.assets.precompile += %w()
 
-# Add additional assets to the asset load path
-# Rails.application.config.assets.paths << Emoji.images_path
-
-# Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
+  # http://stackoverflow.com/questions/23422522/how-to-specify-asset-host-in-rails-4
+  config.action_controller.asset_host = SC.url.root
+end
