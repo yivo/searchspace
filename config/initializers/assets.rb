af8d97b4506dfb975d9301ecc0162392cbdd01d5
Yaroslav Konoplov <eahome00@gmail.com>
2017-06-26 16:50:06 +0300

    Update asset lookup paths: 1. Add app/assets/templates to paths (JST) 2. Don't directly lookup bower_components, require prefix – bower_components e.g. bower_components/jquery/dist/jquery vs jquery/dist/jquery

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,6 +3,12 @@ Rails.application.configure do
   config.assets.version                 = '1.0'
   config.assets.check_precompiled_asset = false
 
+  config.assets.paths << Rails.root.join('app/assets/templates')
+  config.assets.paths << Rails.root.join('vendor/assets')
+  config.assets.paths.map!(&:to_s)
+  config.assets.paths.uniq!
+  config.assets.paths.delete(Rails.root.join('vendor/assets/bower_components').to_s)
+
   # Precompile additional assets.
   config.assets.precompile += %w()
 
