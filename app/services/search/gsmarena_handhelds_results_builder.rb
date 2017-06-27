c9a899d75e9499d1f93f7829531a49c299f2e07a
Yaroslav Konoplov <eahome00@gmail.com>
2017-06-27 18:23:09 +0300

    Add Search::GSMArenaHandheldsResultsBuilder

---
+++ app/services/search/gsmarena_handhelds_results_builder.rb
@@ -0,0 +1,16 @@
+class Search::GSMArenaHandheldsResultsBuilder < Search::ResultsBuilder
+  def build(nokogiri_html_fragment)
+    nokogiri_html_fragment.css('#review-body ul li').map do |el|
+
+      # Transform <br> into new lines.
+      el.css('br').each { |br| br.replace("\n") }
+
+      @source.create_result do |klass|
+        name      = el.text.squish
+        page_url  = URI.join('http://www.gsmarena.com/results.php3', el.css('a')[0]['href']).to_s
+        image_url = el.css('img')[0]['src']
+        klass.new(name, @source, page_url: page_url, image_url: image_url)
+      end
+    end
+  end
+end
