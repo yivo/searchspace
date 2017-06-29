855c87c11dec7ba5305f301b51eefd5ea4bcbb44
Yaroslav Konoplov <eahome00@gmail.com>
2017-06-29 18:26:43 +0300

    Describe helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,7 @@
 module ApplicationHelper
+
+  # Reads image from disk and returns contents.
+  # relative_path must be relative to app/assets/images
   def embed_svg(relative_path)
     File.read(Rails.root.join('app/assets/images', relative_path)).html_safe
   end
